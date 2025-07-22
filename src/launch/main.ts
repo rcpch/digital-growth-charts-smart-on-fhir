a00a1b56285742ae97b082ff557648557d210b76
Michael Barton <michael.barton@rcpch.ac.uk>
2025-07-22 15:18:55 +0100

    Demo launch

---
+++ src/launch/main.ts
@@ -0,0 +1,19 @@
+import smart from 'fhirclient';
+
+console.log("RCPCH Growth Charts - SMART on FHIR launch");
+
+smart.oauth2.authorize({
+    clientId: "my_web_app",
+
+    // The scopes that you request from the EHR. In this case we want to:
+    // launch            - Get the launch context
+    // openid & fhirUser - Get the current user
+    // patient/*.read    - Read patient data
+    scope: "launch openid fhirUser patient/*.read",
+
+    // Typically, if your redirectUri points to the root of the current directory
+    // (where the launchUri is), you can omit this option because the default value is
+    // ".". However, some servers do not support directory indexes so "." and "./"
+    // will not automatically map to the "index.html" file in that directory.
+    redirectUri: "index.html"
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        launch: resolve(__dirname, 'launch.html'),
+      },
+    },
+  },
 })
