4759d19fc92b4d2b72dfa6ae0e889f119414f048
Michael Barton <michael.barton@rcpch.ac.uk>
2025-07-23 14:33:10 +0100

    SDS

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,38 @@ import './App.css';
 import { useState } from 'react';
 
 function App({ title, sex, measurements }: { title: string, sex: 'male' | 'female', measurements: any }) {
-  const [measurementMethod, setMeasurementMethod] = useState<'height' | 'weight'>('height');
+  const [view, setView] = useState<'height_centile' | 'weight_centile' | 'sds'>('height_centile');
+
+  let measurementMethod: any = 'height';
+  let chartType: 'centile' | 'sds' = 'centile';
+  
+  switch (view) {
+    case 'height_centile':
+      measurementMethod = 'height';
+      break;
+    case 'weight_centile':
+      measurementMethod = 'weight';
+      break;
+    case 'sds':
+      chartType = 'sds';
+      // all measurements plotted anyway
+      break;
+  }
 
   return <>
     <div>
-      <button onClick={() => setMeasurementMethod('height')}>Height</button>
-      <button onClick={() => setMeasurementMethod('weight')}>Weight</button>
+      <label>
+        <input type="radio" value="height_centile" checked={view === 'height_centile'} onChange={() => setView('height_centile')} />
+        Height
+      </label>
+      <label>
+        <input type="radio" value="weight_centile" checked={view === 'weight_centile'} onChange={() => setView('weight_centile')} />
+        Weight
+      </label>
+      <label>
+        <input type="radio" value="sds" checked={view === 'sds'} onChange={() => setView('sds')} />
+        SDS
+      </label>
     </div>
     <RCPCHChart
         // Workaround charts bug (https://github.com/rcpch/digital-growth-charts-react-component-library/issues/168)
@@ -20,7 +46,7 @@ function App({ title, sex, measurements }: { title: string, sex: 'male' | 'femal
         measurements={measurements}
         theme={'traditional'}
         enableZoom
-        chartType={'centile'}
+        chartType={chartType}
         enableExport={false}
         exportChartCallback={()=>{}} // this is a callback for the export chart function if true
         clinicianFocus={false}
