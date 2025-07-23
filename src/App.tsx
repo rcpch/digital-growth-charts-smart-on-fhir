60dce8ab5bbe6c572b665eb54505731fb069a739
Michael Barton <michael.barton@rcpch.ac.uk>
2025-07-23 14:02:26 +0100

    Try to switch but it's crashing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { RCPCHChart } from '@rcpch/digital-growth-charts-react-component-library';
 import './App.css';
+import { useState } from 'react';
 
 function App({ title, sex, measurements }: { title: string, sex: 'male' | 'female', measurements: any }) {
-  return (
+  const [measurementMethod, setMeasurementMethod] = useState<'height' | 'weight'>('height');
+
+  return <>
+    <div>
+      <button onClick={() => setMeasurementMethod('height')}>Height</button>
+      <button onClick={() => setMeasurementMethod('weight')}>Weight</button>
+    </div>
     <RCPCHChart
         reference={'uk-who'}
-        measurementMethod={'height'}
+        measurementMethod={measurementMethod}
         sex={sex}
         title={title}
-        measurements={measurements}
+        measurements={{ [measurementMethod]: measurements[measurementMethod] || [] }}
         theme={'traditional'}
         enableZoom
         chartType={'centile'}
@@ -16,7 +23,7 @@ function App({ title, sex, measurements }: { title: string, sex: 'male' | 'femal
         exportChartCallback={()=>{}} // this is a callback for the export chart function if true
         clinicianFocus={false}
     />
-  )
+  </>;
 }
 
 export default App
