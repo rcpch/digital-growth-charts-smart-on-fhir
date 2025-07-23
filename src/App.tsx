bbd4cfd331a3496ed3e0b80aba319c2cbf7ffecc
Michael Barton <michael.barton@rcpch.ac.uk>
2025-07-23 12:26:34 +0100

    Add chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { RCPCHChart } from '@rcpch/digital-growth-charts-react-component-library'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <RCPCHChart
+        reference={'uk-who'}
+        measurementMethod={'height'}
+        sex={'female'}
+        title={"Arthur Scargill - 12345678A"}
+        measurements={{
+          "height": []
+        }}
+        theme={'traditional'}
+        enableZoom
+        chartType={'centile'}
+        enableExport={false}
+        exportChartCallback={()=>{}} // this is a callback for the export chart function if true
+        clinicianFocus={false}
+    />
   )
 }
 
